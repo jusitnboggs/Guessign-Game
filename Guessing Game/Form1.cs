b901651fca7203d86c2836024597ecaa3c6fef2a
jusitnboggs <justin.boggs@yahoo.com>
2015-09-17 21:08:16 -0700

    Update 5

--- Guessing Game/Form1.cs
+++ Guessing Game/Form1.cs
@@ -43,44 +43,58 @@ namespace Guessing_Game
 
         private void submit_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (theNumber == Convert.ToInt32(InputBox.Text))
+                {
+                    MessageBox.Show("Guess is correct - you win!");
+                    wrong = false;
+
+                    DialogResult result = MessageBox.Show("Would you like to play again?",
+                    "Play Again", MessageBoxButtons.YesNo);
+
+                    if (result.ToString().ToLower() == "yes")
+                    {
+                        InputBox.Clear();
+                        InputBox.Text = myRandom.Next(10).ToString();
+                        theNumber = myRandom.Next(10);
+                        MessageBox.Show("Lets see if random and guess random!");
+                    }
+
+                    else if (result.ToString().ToLower() == "no")
+                    {
+                        //add want ever makes it close...
+                        Application.Exit();
+
+                    }
+
+
+                }
+
+                if (theNumber > Convert.ToInt32(InputBox.Text))
+                {
+                    MessageBox.Show("Guess is too low, please try again"); // added + theNumber for debug
+                                                                           //wrong = true;
+                }
+
+                if (theNumber < Convert.ToInt32(InputBox.Text))
+                {
+                    MessageBox.Show("Guess is too high, please try again ");
+                    // wrong = true;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //Show error
+                MessageBox.Show("Please enter a valid number!\nMessage: " + ex.Message);
+
+                //Clear values from textboxes
+                InputBox.Clear();
+                InputBox.Text = myRandom.Next(10).ToString(); // Adds a random number 
+
+            }
             
-            if (theNumber == Convert.ToInt32(InputBox.Text))
-            {
-                MessageBox.Show("Guess is correct - you win!");
-                wrong = false;
-
-                DialogResult result = MessageBox.Show("Would you like to play again?",
-                "Play Again", MessageBoxButtons.YesNo);
-
-                if (result.ToString().ToLower() == "yes")
-                {
-                    InputBox.Clear();
-                    InputBox.Text = myRandom.Next(10).ToString();
-                    theNumber = myRandom.Next(10);
-                    MessageBox.Show("Lets see if random and guess random!");
-                }
-
-                else if (result.ToString().ToLower() == "no")
-                {
-                    //add want ever makes it close...
-                    Application.Exit();
-                    
-                }
-
-
-            }
-
-            if (theNumber > Convert.ToInt32(InputBox.Text))
-            {
-                MessageBox.Show("Guess is too low, please try again"); // added + theNumber for debug
-                //wrong = true;
-            }
-
-            if (theNumber < Convert.ToInt32(InputBox.Text))
-            {
-                MessageBox.Show("Guess is too high, please try again ");
-                // wrong = true;
-            }
 
         }
     }
